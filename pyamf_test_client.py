93af45a0acca00891f921ab0b6428628abecd885
meemeer <meemeer@colinux.localdomain>
2010-09-30 20:30:03 +0900

    pyamf_test_client.py update

--- pyamf_test_client.py
+++ pyamf_test_client.py
@@ -4,8 +4,6 @@ import logging
 from pyamf.remoting.client import RemotingService
 
 client = RemotingService('http://localhost:8080/gateway')
-#client = RemotingService('http://meemeer-lab.appspot.com/gateway')
 service = client.getService('service')
 
-#print service.echo('pyamf test')
 print service.echo('pyamf test')
